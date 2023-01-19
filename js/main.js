de356412759990cdf1cfbed2f8c20d0bc5ff0c4e
Shoha-N1 <abduvohidovs85@gmail.com>
2023-01-19 18:15:17 +0500

    Commit

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ let API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
 let IMG_URL = `http://img.omdbapi.com/?apikey=${API_KEY}`;
 
 let elForm = document.querySelector("[data-form");
-let elList = document.querySelector("[data-movie-list]");
+let elList = document.querySelector("[data-list]");
 let elDIvInfo = document.querySelector("[data-div-info]")
 let elInfo = document.querySelector("[data-info]")
 let elLi = document.querySelector("[data-li]")
 let elDivLi = document.querySelector("[data-div-li]")
 let elTemplate = document.querySelector("[data-template]")
+let elButton = document.querySelector('[data-modal-open]')
 
 
 elForm.addEventListener("submit", (evt) => {
@@ -18,9 +19,11 @@ elForm.addEventListener("submit", (evt) => {
   let name = formData.get("input-name");
 
   searchMovies(name);
-  elList.prepend(createDiv(movie))
+  elList.prepend(createDiv(movie));
   
+ 
 });
+
 
 async function searchMovies(query) {
   elList.innerHTML = `<h1 class="h1">Loading...</h1>`;
@@ -28,35 +31,33 @@ async function searchMovies(query) {
   let searchResult = await res.json();
 
   renderMovies(searchResult.Search);
+  
+  
 }
+
 
 function renderMovies(movies) {
   elList.innerHTML = "";
-  let html = "";
-  movies.forEach((movie) => {
-    html += `<li> <div data-div-li><img src= "${movie.Poster}" alt="${movie.Title}" /> <button type="button" data-modal-open ="#test-modal" class="button-01">Info</button> </div> <h3>${movie.Title}</h3> </li>`;
-   
-    renderInfo(movie)
-    
-  });
-  elList.innerHTML = html;
-}
-
-
-function renderInfo(movie){
-  
- elDIvInfo.innerHTML = elInfo.textContent = `${movie.Title} , ${movie.Type} , ${movie.imdbID}`;
+  let html = ""
+  movies.forEach(movie => {
+    html = elLi =  elInfo.textContent = ` Name = "${movie.Title}" , Type = "${movie.Type}" , Movie_Id = "${movie.imdbID}" ;  ${movie.Poster}`;
+  })
+  movies.filter((movie) => elList.append(createDiv(movie))) 
+  elList = html
 }
 
 
 function createDiv(movie) {
   
-  let elCard = elTemplate.content.cloneNode(true);
- 
-
-  elCard.querySelector("[data-img]").src = `${IMG_URL}`;
-  elCard.querySelector("[data-name]").textContent = movie.Title;
- 
-  return elCard
   
+  let elCard = elTemplate.content.cloneNode(true);
+
+  elCard.querySelector("img").src = movie.Poster;
+  elCard.querySelector("h3").textContent = movie.Title;
+
+  return elCard
+   
 }
+
+
+
